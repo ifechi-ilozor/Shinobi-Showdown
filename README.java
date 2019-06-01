d46ab066fe8e545173048d9f4c8f64d54745d609
Ifechi Ilozor <40251709+ifechi-ilozor@users.noreply.github.com>
2019-05-31 21:50:50 -0400

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,3 @@
 
+Fighting game that allows for multiplayer and player v. computer capabilities, utilizes searching algorithm to simulate an AI 
+experience, and includes extra features such as selecting characters, life points bars, and power up bars.

da729145f406c20bf8b9a225ef445d11024c7dc9
Ifechi Ilozor <40251709+ifechi-ilozor@users.noreply.github.com>
2019-05-31 22:00:21 -0400

    Update README.java

--- README.java
+++ README.java
@@ -1,3 +1,18 @@
-
-Fighting game that allows for multiplayer and player v. computer capabilities, utilizes searching algorithm to simulate an AI 
+# Shinobi-Showdown
+Naruto fighting game that you can play on your own or with friends!
+  
+Allows for multiplayer and player v. computer capabilities, utilizes searching algorithm to simulate an AI 
 experience, and includes extra features such as selecting characters, life points bars, and power up bars.
+
+There are two characters: Naruto and Sasuke (more characters yet to be added), and either can be selected as 
+the human or computer player. They can move in all directions, and they move between squares of a hidden 2-D array.
+Each character has their own regular attack and super attacks (Fireball Jutsu for Sasuke and Razengan for Naruto), 
+but super attacks can only be used every 20 seconds (shown by respective power up bars).
+
+Built in Eclipse.photon
+
+   
+   
+  
+
+  
